ef9ca19ec3c65a81c0287a15f3a7bf83d3ca7beb
amirul-5syafiq <amirul5syafiq@gmail.com>
2022-04-11 12:22:35 +0800

    Updates on PlayScreen

--- frontend/src/pages/playscreen.js
+++ frontend/src/pages/playscreen.js
@@ -1,32 +1,131 @@
 import { Link } from "react-router-dom";
-import playButtonImage from "../assets/img/btn-play.png";
 import classes from "./playscreen.module.css";
 
 function PlayScreen() {
-    return (
-        <div>
-            <div className={classes.bgplayScreen}></div>
-            <div className={classes.ha}></div>
-            <div className={classes.firework}></div>
 
-            
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}>
-                <Link to='/Home'>
-                    <img
-                        id='play'
-                        // className={classes.btnPlay}
-                        // src={playButtonImage}
-                        alt='Play' />
-                </Link>
-            </div>
-        </div>
 
-    )
+
+	return (
+		<div>
+			<div className={classes.container}
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					alignContent: "center",
+				}}>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						alignContent: "center",
+					}}>
+				</div>
+			</div>
+
+
+			<div className={classes.bgplayScreen}>
+
+				<div className={classes.ha}>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							alignContent: "center"
+						}}>
+					</div>
+				</div>
+				<div className={classes.firework}>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							alignContent: "center"
+						}}>
+					</div>
+				</div>
+			</div>
+			{/* <div className={classes.ha}></div> */}
+			{/* <div className={classes.firework}></div> */}
+
+			<div className={classes.gamebanner}>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}>
+
+					<br />
+					<br />
+					<br />
+					<br />
+					<div className={classes.Banner}>
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								alignContent: "center",
+							}}>
+
+							<br />
+						</div>
+					</div>
+				</div>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+					}}>
+					<br />
+				</div>
+			</div>
+
+			{/* <div className={classes.rayabanner}>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+
+					}}>
+					<div className={classes.ha}>
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "center",
+								alignItems: "center",
+								alignContent: "center"
+							}}>
+					</div>
+					</div>
+				</div>
+			</div> */}
+
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}>
+				<Link to='/Home'>
+					<img
+						id='play'
+						// className={classes.btnPlay}
+						// src={playButtonImage}
+						alt='Play' />
+				</Link>
+			</div>
+
+
+		</div>
+
+	)
 }
 
 export default PlayScreen
